6a365724888148e7a544870588839010eaeaf511
cafe24 ksoo <hsnks100@gmail.com>
2017-05-30 16:10:35 +0900

    server

--- githook.php
+++ githook.php
@@ -1,5 +1,10 @@
 <?php
+$error = shell_exec('git pull origin master');
 
-exec("git pull origin master");
+$log_file = fopen("./gitlog.txt", "a");  
+fwrite($log_file, $error."\r\n");  
+fclose($log_file);  
+
+echo($log_file);
 
 ?>
